b74faae78c73bd3d9099e283511db20a56d7d143
midwesty <144859987+midwesty@users.noreply.github.com>
2026-04-21 20:23:04 -0500

    v0.001
    just a few tweaks to launch!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+function htmlEscape(value = "") {
+  return String(value)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#39;");
+}
+
 window.GrabLabMain = (() => {
   const CFG = window.GrabLabConfig;
   const U = window.GrabLabUtils;
